186aeb81e27fd3a3132033676b80b8a9909153c6
Khushbu Verma <vkhushbu874@gmail.com>
2025-10-07 11:53:47 +0530

    Add files via upload

---
+++ primeforloop.py
@@ -0,0 +1,9 @@
+#WAP to check prime number entered through keyboard?
+num=int(input("enter the num="))
+for i in range(2,num):
+    if num%i==0:
+        print("not prime")
+        break
+else:
+    print("prime")
+        
